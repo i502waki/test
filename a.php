6795f31c958fa2ddf1b99bf0e46365a3f8496e20
i502waki <138637973+i502waki@users.noreply.github.com>
2023-07-25 16:47:49 +0900

    a.php

--- a.php
+++ a.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php 
-  foreach(file('/usr/share/dict/words') as $line){
+  foreach(file('/usr/share/dict/linux.words') as $line){
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
     if(strpos($line, "xy") !== false){
      echo "$line";
